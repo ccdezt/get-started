ea3f4f19fa020789357177e0b225cb2927496179
root <root@localhost.localdomain>
2017-10-12 08:15:59 -0400

    Fixed HTML string

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def hello():
 
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}" \
+           "<b>Visits:</b> {visits}<br/>" \
            "<b>help:</b> this should work right {cc}?" 
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits, cc=os.getenv("NAME","compc" )
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits, cc=os.getenv("NAME","compc"))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
